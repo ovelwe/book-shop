eb7fed819223d1e935f54b5a2cbe3cafc6360b28
ovelwe <alekpoli139@gmail.com>
2024-12-27 12:18:51 +0300

    home page

---
+++ components/shared/filters.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import { FilterCheckbox, Title, CheckboxFiltersGroup } from './index';
+import { Input } from '@/components/ui/input';
+import { RangeSlider } from '../ui';
+
+interface Props {
+  className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+  return (
+    <div className={className}>
+      <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
+
+      <div className="flex flex-col gap-4">
+        <FilterCheckbox text="Со скидкой" value="1" />
+        <FilterCheckbox text="Новинки" value="2" />
+        <FilterCheckbox text="В наличии" value="3" />
+      </div>
+
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={5000} defaultValue={0} />
+          <Input type="number" min={100} max={5000} placeholder="5000" />
+        </div>
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
+      </div>
+
+      <CheckboxFiltersGroup
+        className="mt-5"
+        title="Авторы"
+        limit={6}
+        defaultItems={[
+          {
+            text: 'Элизабет Гилберт',
+            value: '1',
+          },
+          {
+            text: 'Ник Вуйчич',
+            value: '2',
+          },
+          {
+            text: 'Светлана Алексиевич',
+            value: '3',
+          },
+          {
+            text: 'Джаннетт Уоллс',
+            value: '4',
+          },
+          {
+            text: 'Дженнифер Уорф',
+            value: '5',
+          },
+          {
+            text: 'Мршавко Штапич',
+            value: '6',
+          },
+        ]}
+        items={[
+          {
+            text: 'Элизабет Гилберт',
+            value: '7',
+          },
+          {
+            text: 'Ник Вуйчич',
+            value: '8',
+          },
+          {
+            text: 'Светлана Алексиевич',
+            value: '9',
+          },
+          {
+            text: 'Джаннетт Уоллс',
+            value: '10',
+          },
+          {
+            text: 'Дженнифер Уорф',
+            value: '11',
+          },
+          {
+            text: 'Мршавко Штапич',
+            value: '12',
+          },
+          {
+            text: 'Элизабет Гилберт',
+            value: '13',
+          },
+          {
+            text: 'Ник Вуйчич',
+            value: '14',
+          },
+          {
+            text: 'Светлана Алексиевич',
+            value: '15',
+          },
+          {
+            text: 'Джаннетт Уоллс',
+            value: '16',
+          },
+          {
+            text: 'Дженнифер Уорф',
+            value: '17',
+          },
+          {
+            text: 'Мршавко Штапич',
+            value: '18',
+          },
+        ]}
+      />
+    </div>
+  );
+};
---
+++ components/shared/header.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { cn } from '@/lib/utils';
+import { Container } from './container';
+import Image from 'next/image';
+import { Button } from '../ui';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+
+interface Props {
+    className?: string;
+}
+
+export const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn('border-b border-gray-100', className)}>
+    <Container className="flex items-center justify-between py-8">
+      <div className="flex items-center gap-4">
+        <Image src="/logo.svg" width={35} height={35} alt="Logo" />
+        <div>
+          <h1 className="text-2xl uppercase font-black">Book Shop</h1>
+          <p className="text-sm text-[#7E6343] leading-3">книги уже ждут тебя</p>
+        </div>
+      </div>
+
+      <div className='flex items-center gap-3'>
+        <Button variant="outline" className='flex items-center gap-1'>
+        <User size={16} />
+          Войти
+        </Button>
+
+        <div>
+          <Button className='group relative'>
+              <b>52 ₽</b>
+              <span className='h-full w-[1px] bg-white/30 mx-3'></span>
+              <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+                <ShoppingCart className='h-4 w-4 relative' strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+          </Button>
+        </div>
+      </div>
+      </Container>
+      </header>
+  );
+};
