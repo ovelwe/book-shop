47efb5f7491615d179256ab275535261c6d8b6f8
ovelwe <alekpoli139@gmail.com>
2024-12-27 13:04:50 +0300

    cards group

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import {Container, Title, TopBar, Filters} from '../components/shared/index'
+import { ProductCard } from '../components/shared/product-card';
+import { ProductsGroupList } from '../components/shared/products-group-list';
 
 export default function Home() {
   return (
@@ -8,14 +10,31 @@ export default function Home() {
     </Container>
       <TopBar />
     <Container className='mt-10 pb-14'>
-      <div className='flex gap-[60px]'>
+      <div className='flex gap-[80px]'>
         <div className='w-[250px]'>
           <Filters />
         </div>
 
         <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Список товаров
+              <ProductsGroupList title='Романы' items={[{
+                id: 1,
+                name: 'Сырный Цыпленок',
+                imageUrl: 'https://content.img-gorod.ru/pim/products/images/36/28/018f5cba-155b-7131-af0f-af3bb4bd3628.jpg?width=200&height=280&fit=bounds',
+                price: 5200,
+              },
+              {
+                id: 2,
+                name: 'Азбука СВО',
+                imageUrl: 'https://pdmnews.ru/wp-content/uploads/2023/04/enoeno.jpg',
+                price: 1488,
+              },
+              {
+                id: 3,
+                name: 'Цитатник Стэтхэма',
+                imageUrl: 'https://ir-7.ozone.ru/s3/multimedia-1-6/wc1000/7148100390.jpg',
+                price: 666,
+              }]} categoryId={1}/>
             </div>
           </div>
       </div>
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -4,6 +4,7 @@ export {Title} from './title';
 export {Categories} from './categories';
 export {SortPopup} from './sort-popup'
 export {TopBar} from './top-bar';
-export {Filters} from './filters'
-export {FilterCheckbox} from './filter-checkbox'
-export {CheckboxFiltersGroup} from './checkbox-filters-group'
+export {Filters} from './filters';
+export {FilterCheckbox} from './filter-checkbox';
+export {CheckboxFiltersGroup} from './checkbox-filters-group';
+export {ProductCard} from './product-card';
